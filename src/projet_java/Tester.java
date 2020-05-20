d2ca634f830f3d8bf40b93cef66cfcd2471ba460
Kevinkoy <koy-kevin@hotmail.fr>
2020-05-21 00:28:51 +0200

    K2 Push: Ajout de la Javadoc (Jeudi 21 Mai 00h28) Merge origin/master

--- src/projet_java/Tester.java
+++ src/projet_java/Tester.java
@@ -1,34 +1,44 @@
 package projet_java;
+
 import Modele.*;
 import Modele.MaDate;
 
 //import java.util.*;
 import java.sql.*;
 // TEST
+
 /**
  *
- * @author KévinKOY, AliceCABANE, SylvainLOUCHE, Student at ECE PARIS, PROJET JAVA 2020 ING3_TD4 
+ * @author KévinKOY, AliceCABANE, SylvainLOUCHE, Student at ECE PARIS, PROJET
+ * JAVA 2020 ING3_TD4
  */
-public class Tester
-{
+public class Tester {
 
-    public static void main(String[] args) 
-    {           
-        // DECLARATION
-        Seance maSeance;
-        Time time;
-        Time duree;
-        MaDate date;
- 
-        // Intialisation
-        time = new Time(0,0,0);
-        duree = new Time(1,30,0);
-        date =  new MaDate(21, 5, 1999);
-        maSeance = new Seance(4,date, time, duree, 2, 56, 2);
-        
+    public static void main(String[] args) {
+        // DECLARATION && INITIALISATION
+        int id_seance = 1;
+
+        int jour = 20;
+        int mois = 05;
+        int annee = 2020;
+        MaDate date = new Modele.MaDate(jour, mois, annee);
+
+        Time heure_debut = new Time(0, 0, 0);
+        Time heure_fin = new Time(1, 30, 0);
+        int etat = 0; // 0 default en cours de validation | 1 valide
+
+        int id_cours = 1;
+        String nom_cours = "Mathematiques";
+        Cours cours = new Modele.Cours(id_cours, nom_cours);
+
+        int id_type = 1;
+        String nom_type = "Cours Magistral";
+        Type_cours type_cours = new Modele.Type_cours(id_type, nom_type);
+
+        Seance maSeance = new Modele.Seance(id_seance, date, heure_debut, heure_fin, etat, cours, type_cours);
+
         /// Display
         System.out.print(maSeance.toString());
 
     }
-    
 }
