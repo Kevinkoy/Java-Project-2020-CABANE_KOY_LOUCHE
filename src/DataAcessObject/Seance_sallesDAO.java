44545deed573116a9a7d840e9e3124bcbbcda3f1
Pere-Siffleur <47555393+Pere-Siffleur@users.noreply.github.com>
2020-06-07 22:38:32 +0200

    Add files via upload

--- src/DataAcessObject/Seance_sallesDAO.java
+++ src/DataAcessObject/Seance_sallesDAO.java
@@ -1,8 +1,14 @@
 package DataAcessObject;
 
+import Modele.Cours;
+import Modele.Salle;
+import Modele.Seance;
 import Modele.Seance_salles;
+import Modele.Site;
+import Modele.Type_cours;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 
@@ -95,8 +101,132 @@ public class Seance_sallesDAO extends DAO<Seance_salles> {
     }
 
     @Override
-    public Seance_salles find(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Seance_salles find(int id_seance) {
+        /// CONSTRUCTEUR NULL (PAR DEFAUT)
+        Seance_salles returned = new Seance_salles();
+        try {
+            // REQUETE
+            String sql = "SELECT * FROM `seance_salles`\n"
+                    + "INNER JOIN `seance`ON seance_salles.ID_Seance = seance.ID\n"
+                    + "INNER JOIN `cours`ON seance.ID_Cours = cours.ID\n"
+                    + "INNER JOIN `type_cours`ON seance.ID_Type = type_cours.ID\n"
+                    + "INNER JOIN `salle`ON seance.ID_Salle = salle.ID\n"
+                    + "INNER JOIN `site`ON salle.ID_Site = site.ID\n"
+                    + "WHERE seance_salles.ID_Seance ='" + id_seance + "';";
+            // PrepareStatement
+            PreparedStatement preparestatement = this.connection.prepareStatement(sql);
+            // ResultSet
+            ResultSet result = preparestatement.executeQuery(sql);
+
+            // SI RESULTAT...
+            if (result.next()) {
+                /// return = RESULTAT
+                Seance seance = new Seance();
+                seance.setId(result.getInt("seance_salles.ID_Seance"));
+                seance.setSemaine(result.getInt("seance.Semaine"));
+                seance.setDate(result.getDate("seance.Date"));
+                seance.setHeure_debut(result.getTime("seance.Heure_Debut"));
+                seance.setHeure_fin(result.getTime("seance.Heure_Fin"));
+                seance.setEtat(result.getInt("seance.Etat"));
+                /// ***
+                Cours cours = new Cours();
+                cours.setId(result.getInt("seance.ID_Cours"));
+                cours.setNom(result.getString("cours.Nom"));
+                /// *** 
+                seance.setCours(cours);
+
+                ///***
+                Type_cours type_cours = new Type_cours();
+                type_cours.setId(result.getInt("seance.ID_Type"));
+                type_cours.setNom("type_cours.Nom");
+                ///*** 
+                seance.setType_cours(type_cours);
+
+                ///***
+                Salle salle = new Salle();
+                salle.setId(result.getInt("seance_salles.ID_Salle"));
+                salle.setNom(result.getString("salle.Nom"));
+                salle.setCapacite(result.getInt("salle.Capacite"));
+
+                ///***
+                Site site = new Site();
+                site.setId(result.getInt("salle.ID_Site"));
+                site.setNom("site.Nom");
+
+                salle.setSite(site);
+
+                returned = new Seance_salles(seance, salle);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }
+        // return soit NULL || soit RESULTAT...
+        return returned;
+    }
+
+    public Seance_salles find_by_id_salle(int id_salle) {
+        /// CONSTRUCTEUR NULL (PAR DEFAUT)
+        Seance_salles returned = new Seance_salles();
+        try {
+            // REQUETE
+            String sql = "SELECT * FROM `seance_salles`\n"
+                    + "INNER JOIN `seance`ON seance_salles.ID_Seance = seance.ID\n"
+                    + "INNER JOIN `cours`ON seance.ID_Cours = cours.ID\n"
+                    + "INNER JOIN `type_cours`ON seance.ID_Type = type_cours.ID\n"
+                    + "INNER JOIN `salle`ON seance.ID_Salle = salle.ID\n"
+                    + "INNER JOIN `site`ON salle.ID_Site = site.ID\n"
+                    + "WHERE seance_salles.ID_Salle ='" + id_salle + "';";
+            // PrepareStatement
+            PreparedStatement preparestatement = this.connection.prepareStatement(sql);
+            // ResultSet
+            ResultSet result = preparestatement.executeQuery(sql);
+
+            // SI RESULTAT...
+            if (result.next()) {
+                /// return = RESULTAT
+                Seance seance = new Seance();
+                seance.setId(result.getInt("seance_salles.ID_Seance"));
+                seance.setSemaine(result.getInt("seance.Semaine"));
+                seance.setDate(result.getDate("seance.Date"));
+                seance.setHeure_debut(result.getTime("seance.Heure_Debut"));
+                seance.setHeure_fin(result.getTime("seance.Heure_Fin"));
+                seance.setEtat(result.getInt("seance.Etat"));
+                /// ***
+                Cours cours = new Cours();
+                cours.setId(result.getInt("seance.ID_Cours"));
+                cours.setNom(result.getString("cours.Nom"));
+                /// *** 
+                seance.setCours(cours);
+
+                ///***
+                Type_cours type_cours = new Type_cours();
+                type_cours.setId(result.getInt("seance.ID_Type"));
+                type_cours.setNom("type_cours.Nom");
+                ///*** 
+                seance.setType_cours(type_cours);
+
+                ///***
+                Salle salle = new Salle();
+                salle.setId(result.getInt("seance_salles.ID_Salle"));
+                salle.setNom(result.getString("salle.Nom"));
+                salle.setCapacite(result.getInt("salle.Capacite"));
+
+                ///***
+                Site site = new Site();
+                site.setId(result.getInt("salle.ID_Site"));
+                site.setNom("site.Nom");
+
+                salle.setSite(site);
+
+                returned = new Seance_salles(seance, salle);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            //e.printStackTrace();
+        }
+        // return soit NULL || soit RESULTAT...
+        return returned;
     }
 
 }
